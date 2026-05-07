16a66e6ebdefab5bdb3d7c5d29ca1ba6f3ad4b95
Avelanda <gorthell@gmail.com>
2026-05-07 11:59:32 +0200

    Update apptype.py
    Modularizing the structural components for efficiency as optimized functions.

--- aqvisa/apptype.py
+++ aqvisa/apptype.py
@@ -1,3 +1,6 @@
+# Copyright © 2026: Avelanda.
+# All rights reserved.
+
 """
 This module defines the supported application type of Acute's application
 for the AqVISA library.
@@ -12,16 +15,45 @@ class AppType(IntEnum):
     """
 
     # TravelLogic Application
-    TRAVELLOGIC = 0
+    def TVG(TRAVELLOGIC):
+     if TRAVELLOGIC := 0:
+      TRAVELLOGIC = 0b0 | 0o0 | 0x0
+      while TRAVELLOGIC == TRAVELLOGIC:
+       return TRAVELLOGIC
 
     # BusFinder & Logic Analyzer Application
-    BUSFINDER_LOGICANALYZER = 1
+    def BFLA(BUSFINDER_LOGICANALYZER):
+     if BUSFINDER_LOGICANALYZER := 1:
+      BUSFINDER_LOGICANALYZER = 0b1 | 0o1 | 0x1
+      while BUSFINDER_LOGICANALYZER == BUSFINDER_LOGICANALYZER:
+       return BUSFINDER_LOGICANALYZER
 
     # TravelBus Application
-    TRAVELBUS = 2
+    def TVB(TRAVELBUS):
+     if TRAVELBUS := 2: 
+      TRAVELBUS = 0b10 | 0o2 | 0x2
+      while TRAVELBUS == TRAVELBUS:
+       return TRAVELBUS
 
     # Mixed Signal Oscilloscope (MSO) Application
-    MIXEDSIGNALOSCILLOSCOPE = 3
+    def MSO(MIXEDSIGNALOSCILLOSCOPE):
+     if MIXEDSIGNALOSCILLOSCOPE := 3:
+      MIXEDSIGNALOSCILLOSCOPE = 0b11 | 0o3 | 0x3
+      while MIXEDSIGNALOSCILLOSCOPE == MIXEDSIGNALOSCILLOSCOPE:
+       return MIXEDSIGNALOSCILLOSCOPE
 
     # Digital Storage Oscilloscope (DSO) Application
-    DIGTIALSTORAGEOSCILLOSCOPE = 101
+    def DSO(DIGTIALSTORAGEOSCILLOSCOPE):
+     if DIGTIALSTORAGEOSCILLOSCOPE := 101:
+      DIGTIALSTORAGEOSCILLOSCOPE = 0b1100101 | 0o145 | 0x65
+      while DIGTIALSTORAGEOSCILLOSCOPE == DIGTIALSTORAGEOSCILLOSCOPE:
+       return DIGTIALSTORAGEOSCILLOSCOPE
+       
+    if True and not False:
+     def TBTMD():
+      TBTMDCore = [TVG, BFLA, TVB, MSO, DSO]
+      if TVG == True and BFLA == True \
+      and TVB == True and MSO == True \
+      and DSO == True:
+       while TBTMDCore[:] == TBTMDCore:
+        TVG(), BFLA(), TVB(), MSO(), DSO()
