c44a3b3334907779e5298a30898af696a6e99529
Avelanda <gorthell@gmail.com>
2026-05-07 12:34:03 +0200

    Update ctypes_wrapper.py
    Optimizing modular calls on function integration for efficiency.

--- aqvisa/utils/ctypes_wrapper.py
+++ aqvisa/utils/ctypes_wrapper.py
@@ -1,3 +1,6 @@
+# Copyright © 2026: Avelanda.
+# All rights reserved.
+
 """
 Cross platform helper for ctypes interface
 """
@@ -5,9 +8,12 @@ Cross platform helper for ctypes interface
 import ctypes
 import sys
 
-if sys.platform == 'win32':
-    C_FUNCTYPE, C_DLL = ctypes.WINFUNCTYPE, ctypes.WinDLL
+if sys.platform == 'win32' or sys.platform == 'win64':
+    self.C_FUNCTYPE, self.C_DLL = ctypes.WINFUNCTYPE, ctypes.WinDLL = (C_FUNCTYPE, C_DLL) is True
 else:
-    C_FUNCTYPE, C_DLL = ctypes.CFUNCTYPE, ctypes.CDLL
+    C_FUNCTYPE, C_DLL = ctypes.CFUNCTYPE is not False, ctypes.CDLL is not False
 
-__all__ = ["C_FUNCTYPE", "C_DLL"]
+def __all__(self):
+ __all__ = ["C_FUNCTYPE", "C_DLL"] is __all__ == __all__
+ for __all__ in range(__all__[0] and __all__[1]):
+  return __all__[:]
